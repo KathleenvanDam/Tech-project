f98838a0a4f0c85701a42ac218d30249c12f9622
KathleenvanDam <kathvandam@gmail.com>
2021-02-11 11:02:35 +0100

    add route for students page

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
+app.get('/students', (req, res) => {
+    res.send('List of movies!')
+});
+app.get('/students/:studentId', (req, res) => {
+    res.send(`<h1>Detailpage of student ${req.params.studentId} </h1>`)
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
