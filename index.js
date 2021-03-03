87c034259b2942615dc3028c8681810503aaee94
KathleenvanDam <kathvandam@gmail.com>
2021-03-03 15:10:12 +0100

    tried to connect db, but errors

--- index.js
+++ index.js
@@ -2,11 +2,31 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv").config();
+const { MongoClient } = require('mongodb');
 const port = 3000;
 
 console.log(process.env.TESTVAR)
 
 const year = ["year 1", "year 2", "year 3", "year 4"];
+
+let db = null;
+// function connectDB
+async function connectDB() {
+  //get URI form env file
+  const uri = process.env.DB_URI
+  //make connection to DB
+  const options = { useUnifiedTopology: true };
+  const client = new MongoClient(uri, options)
+  await client.connect();
+  db = await client.db(process.env.DB_NAME)
+}
+connectDB(); 
+try {
+  console.log('We have made a connection to Mongo!')
+  
+} catch (error) {
+  console.log(error)
+}
 
 app.use(express.static("public"));
 
