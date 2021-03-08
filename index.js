5029bd641bff161f30c8e0bf443c7e58397ff5cd
KathleenvanDam <kathvandam@gmail.com>
2021-03-08 14:24:42 +0100

    change name images

--- index.js
+++ index.js
@@ -68,9 +68,3 @@ app.listen(port, () => {
 });
 
 //FIXME: ESLint dingen laten fixen
-
-  res.render('home', {
-    title: "Studenten",
-    results: filteredStudents.length,
-    students:filteredStudents
-  })
