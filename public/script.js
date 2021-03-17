d75bc2b30f949a7d76579a6f7a59a6da525ecff5
KathleenvanDam <kathvandam@gmail.com>
2021-03-17 16:00:44 +0100

    add active toggle to js file

--- public/script.js
+++ public/script.js
@@ -5,3 +5,10 @@ function slide(){
     let filter = document.querySelector(".filterForm");
     filter.classList.add("filterForm-show");
 }
+
+const likeButton = document.querySelector(".like");
+likeButton.addEventListener("click", like);
+
+function like(){
+     likeButton.classList.toggle('active');
+}
